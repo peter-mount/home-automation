94b3228b2dd4992e75f35b311c3f4abf42685b51
peter <peter@retep.org>
2022-08-18 21:10:41 +0100

    Add documentation about the Geiger counter

--- geiger/geiger.go
+++ geiger/geiger.go
@@ -20,6 +20,10 @@ type Geiger struct {
 }
 
 func (m *Geiger) Start() error {
+	// This sets the baud rate of the connection.
+	// This is the default for the GMC-300 Plus Geiger counter.
+	// You might have to change this for other models.
+	// See GQ-RFC1201.txt for details.
 	mode := &serial.Mode{
 		BaudRate: 115200,
 		DataBits: 8,
@@ -33,11 +37,19 @@ func (m *Geiger) Start() error {
 	}
 	m.port = port
 
-	m.cron.AddTask("0/10 * * * * *", m.getStats)
+	// Poll the Geiger counter every 10 seconds
+	_, err = m.cron.AddTask("0/10 * * * * *", m.getStats)
+	if err != nil {
+		return err
+	}
 
 	return m.getStats(context.Background())
 }
 
+// sendCmd sends a command to the Geiger Counter.
+// cmd is the text content of the command without the prefix or suffix characters.
+// len is the number of bytes expected from the command.
+// len=0 if no response is expected.
 func (m *Geiger) sendCmd(cmd string, len int) ([]byte, error) {
 	c := fmt.Sprintf("<%s>>%c", cmd, 13)
 	//log.Printf("sending %q", c)
@@ -69,6 +81,7 @@ func (m *Geiger) sendCmd(cmd string, len int) ([]byte, error) {
 	return buf, nil
 }
 
+// getStats is a Task that sends the relevant commands to the Geiger counter.
 func (m *Geiger) getStats(ctx context.Context) error {
 	//log.Println("Getting data")
 
@@ -121,10 +134,16 @@ func (m *Geiger) getStats(ctx context.Context) error {
 	return nil
 }
 
+// toInt16 converts a 2 byte value to an unsigned integer
 func toInt16(b []byte) int {
 	return (int(b[0]) * 256) + int(b[1])
 }
 
+// toFloat64 converts a 4 byte value to a signed float64.
+// Byte 0 contains the integer portion.
+// Byte 1 contains the fraction
+// Byte 2 if not 0 indicates the value is negative.
+// Byte 3 is not referenced here but is not used in the protocol
 func toFloat64(b []byte) float64 {
 	temp := float64(b[0]) + (float64(b[1]) / 100)
 	if b[2] != 0 {
